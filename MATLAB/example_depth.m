b5ffb85d4c932ff41eaa6b3c13f6508cda45ce81
anguspcwylie <119887307+anguspcwylie@users.noreply.github.com>
2024-05-13 15:43:19 -0400

    made compatible with new TGSPhase_Analysis time addition

--- MATLAB/example_depth.m
+++ MATLAB/example_depth.m
@@ -30,12 +30,13 @@ theta = 0;
 thetaErr = 0;
 C = 0;
 CErr = 0;
+file_date_time = ' ';
 nominalSpacing = 0;
 formatSpec = '%.1f';
 %Set up the output files with their headers, clears the file at beginning.
 printFile = pname + string('\') + str_base + string('grating_depth_study_3.7um.txt');
 fid1 = fopen( printFile, 'wt' );
-fprintf(fid1,'%s', 'grating_value[um] SAW_freq[Hz] SAW_freq_error[Hz] A[Wm^-2] A_err[Wm^-2] alpha[m^2s^-1] alpha_err[m2s-1] beta[s^0.5] beta_err[s^0.5] B[Wm^-2] B_err[Wm^-2] theta theta_err tau[s] tau_err[s] C[Wm^-2] C_err[Wm^-2]');
+fprintf(fid1,'%s', 'run_name date_time grating_value[um] SAW_freq[Hz] SAW_freq_error[Hz] A[Wm^-2] A_err[Wm^-2] alpha[m^2s^-1] alpha_err[m2s-1] beta[s^0.5] beta_err[s^0.5] B[Wm^-2] B_err[Wm^-2] theta theta_err tau[s] tau_err[s] C[Wm^-2] C_err[Wm^-2]');
 fclose(fid1);
 
 %Run through each of the files, sweeping across gratings and peak-skips
@@ -63,10 +64,10 @@ for i=1:length(gratArr)
     
     disp(['current run is: ', pos_str, '   with grating: ', num2str(gratArr(i)),' um']);
     disp(['current baseline is: ', POSbaselineStr]);
-    [freq,freq_err,speed,diff,diff_err,tau(:,i),tauErr, A, AErr, beta, betaErr, B, BErr, theta, thetaErr, C, CErr]=TGSPhaseAnalysis(pos_str,neg_str,gratArr(i),2,0,1, POSbaselineStr, NEGbaselineStr);
+    [freq,freq_err,speed,diff,diff_err,tau(:,i),tauErr, A, AErr, beta, betaErr, B, BErr, theta, thetaErr, C, CErr, file_date_time]=TGSPhaseAnalysis(pos_str,neg_str,gratArr(i),2,0,1, POSbaselineStr, NEGbaselineStr);
     
     fid1 = fopen( printFile, 'a' );
-    fprintf(fid1, string('\n%.8g %.8g %.8g %.8g %.8g %.8g %.8g %.8g %.8g %.8g %.8g %.8g %.8g %.8g %.8g %.8g %.8g'), gratArr(i), freq, freq_err, A, AErr, diff, diff_err, beta, betaErr, B, BErr, theta, thetaErr, tau(3,i), tauErr, C, CErr);
+    fprintf(fid1, string('\n%s %s %.8g %.8g %.8g %.8g %.8g %.8g %.8g %.8g %.8g %.8g %.8g %.8g %.8g %.8g %.8g %.8g %.8g'), pos_str, file_date_time, gratArr(i), freq, freq_err, A, AErr, diff, diff_err, beta, betaErr, B, BErr, theta, thetaErr, tau(3,3*(i)), tauErr, C, CErr);
     fclose(fid1);
     
 end
