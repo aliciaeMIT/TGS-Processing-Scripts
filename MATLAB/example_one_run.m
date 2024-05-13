ba6d370d24efd8918b40cfb995db89cedc875d42
anguspcwylie <119887307+anguspcwylie@users.noreply.github.com>
2024-05-13 15:53:57 -0400

    made compatible with new TGSPhase_Analysis time addition

--- MATLAB/example_one_run.m
+++ MATLAB/example_one_run.m
@@ -18,7 +18,7 @@ fid1 = fopen( printFile, 'wt' );
 posstr = strcat(pname,string('\'),str_base,'-POS-1.txt');
 negstr = strcat(pname,string('\'),str_base,'-NEG-1.txt');
 
-[freq_final,freq_error,speed,diffusivity,diffusivity_err,tau, tauErr, ParamA, AErr, ParamBeta, BetaErr, paramB, BErr, paramTheta, thetaErr, paramC, CErr] = TGSPhaseAnalysis(posstr,negstr,grat,2,0,blbool,baselinePOS, baselineNEG);
+[freq_final,freq_error,speed,diffusivity,diffusivity_err,tau, tauErr, ParamA, AErr, ParamBeta, BetaErr, paramB, BErr, paramTheta, thetaErr, paramC, CErr, file_date_time] = TGSPhaseAnalysis(posstr,negstr,grat,2,0,blbool,baselinePOS, baselineNEG);
 disp('Frequency = ');
 disp(freq_final);
 disp('Frequency error = ');
@@ -27,9 +27,11 @@ disp('Thermal diffusivity = ');
 disp(diffusivity);
 disp('Thermal diffusivity error = ');
 disp(diffusivity_err);
+disp('File date and time = ');
+disp(file_date_time);
 
 fid1 = fopen( printFile, 'a' );
     naming_str=strcat(str_base,num2str(i));
-    fprintf(fid1, string('\n%s %.8g %.8g %.8g %.8g %.8g %.8g %.8g %.8g %.8g %.8g %.8g %.8g %.8g %.8g %.8g %.8g %.8g'), naming_str, grat, freq_final, freq_error, ParamA, AErr, diffusivity, diffusivity_err, ParamBeta, BetaErr, paramB, BErr, paramTheta, thetaErr, tau(3), tauErr, paramC, CErr);
+    fprintf(fid1, string('\n%s %s %.8g %.8g %.8g %.8g %.8g %.8g %.8g %.8g %.8g %.8g %.8g %.8g %.8g %.8g %.8g %.8g %.8g'), naming_str, file_date_time, grat, freq_final, freq_error, ParamA, AErr, diffusivity, diffusivity_err, ParamBeta, BetaErr, paramB, BErr, paramTheta, thetaErr, tau(3), tauErr, paramC, CErr);
     fclose(fid1);
 %close all % comment this out if you want to keep the plot windows at the end
